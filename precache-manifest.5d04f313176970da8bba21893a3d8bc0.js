aa37173e50b6b4a258ada0bf237b83e31e13d4ac
Mgin <mavangin@gmail.com>
2020-08-17 15:34:48 -0700

    Updates

---
+++ precache-manifest.5d04f313176970da8bba21893a3d8bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d587780f3d5d3f9d66e2255a18aa521",
+    "url": "/mvangin/reactBookLibrary.git/index.html"
+  },
+  {
+    "revision": "e0550cff74e6b1fd7f83",
+    "url": "/mvangin/reactBookLibrary.git/static/css/main.3e2e84a5.chunk.css"
+  },
+  {
+    "revision": "c1eda895227fc4cda354",
+    "url": "/mvangin/reactBookLibrary.git/static/js/2.ca59a247.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mvangin/reactBookLibrary.git/static/js/2.ca59a247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0550cff74e6b1fd7f83",
+    "url": "/mvangin/reactBookLibrary.git/static/js/main.200b81a0.chunk.js"
+  },
+  {
+    "revision": "6d9ce6b93a00f6257f07",
+    "url": "/mvangin/reactBookLibrary.git/static/js/runtime-main.308f9914.js"
+  }
+]);
