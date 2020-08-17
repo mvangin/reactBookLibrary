c2c4fd66a10abe1579a802252fd3558f1924ad39
Mgin <mavangin@gmail.com>
2020-08-17 16:04:15 -0700

    clear form on closing

--- todoapp/src/Form.js
+++ todoapp/src/Form.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./Form.css"
 
 class Form extends React.Component {
-    
+
     constructor() {
         super()
         this.state = {
@@ -11,72 +11,77 @@ class Form extends React.Component {
             genre: "",
             pages: "",
             isRead: false,
-            formDisplay : false
+            formDisplay: false
         }
-        this.handleSubmit =  this.handleSubmit.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleDisplay = this.handleDisplay.bind(this)
     }
 
-    handleSubmit(e){
+    handleSubmit(e) {
         e.preventDefault()
         if (this.state.bookTitle === "" || this.state.author === "" || this.state.genre === ""
-     || this.state.pagse === "") {
+            || this.state.pagse === "") {
             return
-        } 
+        }
 
-        const newBook = {title : this.state.bookTitle, author: this.state.author, 
-        genre: this.state.genre, pages: this.state.pages, isRead: false}
+        const newBook = {
+            title: this.state.bookTitle, author: this.state.author,
+            genre: this.state.genre, pages: this.state.pages, isRead: false
+        }
         this.props.addBook(newBook)
-        this.setState({bookTitle : ""})
-        this.setState({author : ""})
-        this.setState({genre : ""})
-        this.setState({pages : ""})
-        this.setState({formDisplay: false})
-    
+        this.handleDisplay()
     }
 
     handleChange(e) {
-        this.setState({[e.target.name]: e.target.value})
+        this.setState({ [e.target.name]: e.target.value })
     }
 
-    handleDisplay(){
-        this.setState(prevState => ({formDisplay: !prevState.formDisplay}))
+    handleDisplay() {
+        this.setState(prevState => ({ formDisplay: !prevState.formDisplay }))
+        this.clearForm()
+    }
+
+    clearForm() {
+        this.setState({ bookTitle: "" })
+        this.setState({ author: "" })
+        this.setState({ genre: "" })
+        this.setState({ pages: "" })
     }
 
     render() {
         return (
             <>
-            <button className="bookButton" onClick={this.handleDisplay}> Add New Book </button>
-            
-            <div id={this.state.formDisplay ? "formDisplayActive" : "formDisplayHidden"} >
+                <button className="bookButton" onClick={this.handleDisplay}> Add New Book </button>
 
-            <form id="form" onSubmit={this.handleSubmit}>
-                
-                <ul id="inputList">
-                    <div id="cancelForm" onClick={this.handleDisplay}>
-                        +
+                <div id={this.state.formDisplay ? "formDisplayActive" : "formDisplayHidden"} >
+
+                    <form id="form" onSubmit={this.handleSubmit}>
+
+                        <ul id="inputList">
+                            <div id="cancelForm" onClick={this.handleDisplay}>
+                                +
                     </div>
-                    <li>
-                    <input className="formItem" type="text" placeholder="Title" name="bookTitle" value={this.state.bookTitle} onChange={this.handleChange} required />
-                    </li>
+                            <li>
+                                <input className="formItem" type="text" placeholder="Title" name="bookTitle" value={this.state.bookTitle} onChange={this.handleChange} required />
+                            </li>
 
-                    <li>
-                    <input className="formItem" type="text" placeholder="Author" name="author" value={this.state.author} onChange={this.handleChange} required />
-                    </li>
+                            <li>
+                                <input className="formItem" type="text" placeholder="Author" name="author" value={this.state.author} onChange={this.handleChange} required />
+                            </li>
 
-                    <li>
-                    <input className="formItem" type="text" placeholder="Genre" name="genre" value={this.state.genre} onChange={this.handleChange} required />
-                    </li>
+                            <li>
+                                <input className="formItem" type="text" placeholder="Genre" name="genre" value={this.state.genre} onChange={this.handleChange} required />
+                            </li>
 
-                    <li>
-                    <input className="formItem" type="number" placeholder="Pages" name="pages" value={this.state.pages} onChange={this.handleChange} required/>
-                    </li>
+                            <li>
+                                <input className="formItem" type="number" placeholder="Pages" name="pages" value={this.state.pages} onChange={this.handleChange} required />
+                            </li>
 
-                    <button type="submit" className="bookButton"> Submit Note</button>
-                </ul>
-            </form>
-            </div>
+                            <button type="submit" className="bookButton"> Submit Note</button>
+                        </ul>
+                    </form>
+                </div>
             </>
         )
     }
